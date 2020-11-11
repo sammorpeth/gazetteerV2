08279376ba77f799daa91561207867195277a028
dosed91 <sammorpeth@gmail.com>
2020-11-11 17:03:57 +0000

    Added news API and implemented carousel and modal. Need to fix button. Need to add currency. Possibly add photos.

--- libs/js/script.js
+++ libs/js/script.js
@@ -88,7 +88,8 @@ $('#country-select').on('change', function() {
     
     success: function(result) {
     // console.log(result['data']);
-
+      // console.log($('#country-select').val());
+   
       const selectedCountry = result['data'];
       // set country's lat and lng
       const countryLat = selectedCountry[0]['latlng'][0];
@@ -97,6 +98,11 @@ $('#country-select').on('change', function() {
       const weatherLng = selectedCountry['capitalWeather']['coord']['lon'];
       const capitalWiki = selectedCountry['capitalWiki'];
       const capitalWeather = selectedCountry['capitalWeather'];
+      const countryNews = selectedCountry['news']['articles'];
+      console.log(countryNews);
+
+
+     
       
       const countryInfoHTML = `<h2>${selectedCountry[0]['name']}</h2>
                             <ul class="country-info">
@@ -164,6 +170,44 @@ $('#country-select').on('change', function() {
       })
 
       mymap.addLayer(wikiMarkers);
+
+      // News modal
+
+      // Build HTML for news
+
+      // Set a dummy item
+      let newsHTML = `<div class="carousel-item">
+    </div>`;
+
+   
+      if(countryNews.length === 0) {
+         // Default HTML for most countries
+        newsHTML = `It looks like we can't get any news for your chosen country. Please choose a country from the list below:...`
+      } else {
+        countryNews.forEach(article => {
+          newsHTML +=  `<div class="carousel-item">
+          <img class="d-block w-100 news-img" src="${article.urlToImage}" alt="${article.title}">
+          <h2>${article.title}</h2>
+          <a href="${article.url}">Read more...</a>
+        </div>`;
+        })
+      }
+      // Attach controls 
+      newsHTML += ` <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>`
+      // Set HTML 
+      $('.carousel-inner').html(newsHTML);
+      // Remove dummy item
+      $('#news-items div:first-of-type').remove();
+      // Add active class to first item to make it appear
+      $('#news-items div:nth-of-type(2)').addClass('active');
+     
 
 
     },
--- libs/php/restCountries.php
+++ libs/php/restCountries.php
@@ -5,7 +5,16 @@
   // Rest Countries - Country Info
   $infoUrl='https://restcountries.eu/rest/v2/alpha?codes=' . $_REQUEST['countryCode'];
   // $infoUrl='https://restcountries.eu/rest/v2/alpha?codes=GB';
-  
+
+  // flickr api key
+//   d05256dfd2c01b4b9a532e1a8a15ba47
+
+// Secret:
+// 595c7987bb0fe5ef
+
+// Edit app details - Edit auth flow for this app - View all Apps by You
+
+
 
 	$ch1 = curl_init();
 	curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
@@ -46,6 +55,11 @@
   $output['data']['capitalWeather'] = $capitalWeatherDecode; 
   curl_close($ch2);
 
+  // ea1848fa-ca80-4ada-9cf9-3390bc72f3c8
+
+  
+
+
   $capitalLat = $output['data']['capitalWeather']['coord']['lat'];
   $capitalLng = $output['data']['capitalWeather']['coord']['lon'];
 
@@ -64,6 +78,27 @@
   $output['data']['capitalWiki'] = $capitalWikiDecode['geonames']; 
 
   curl_close($ch3);
+
+  // $currencyUrl = 'https://v2.api.forex/infos/currency/GBP.json';
+
+
+  // $flickrKey = 'd05256dfd2c01b4b9a532e1a8a15ba47';
+  // $flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' . $flickrKey.'&lat=55&lon=-1.6&per_page=10&format=json';
+
+  $newsKey = 'a920f9bb0b2f438ea92c53fa19f9a2d9';
+  $newsUrl = 'https://newsapi.org/v2/top-headlines?country='. $_REQUEST['countryCode'].'&apiKey=a920f9bb0b2f438ea92c53fa19f9a2d9&pageSize=5';
+
+  $ch4 = curl_init();
+	curl_setopt($ch4, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch4, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch4, CURLOPT_URL,$newsUrl);
+
+  $newsInfo = curl_exec($ch4);
+  $newsDecode = json_decode($newsInfo,true);
+
+  $output['data']['news'] = $newsDecode; 
+
+  curl_close($ch4);
 
   header('Content-Type: application/json; charset=UTF-8');
 	
